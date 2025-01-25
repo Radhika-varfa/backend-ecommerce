ff0592179bfd9738b8b14add68af8a2faf74446c
biztechadvisors <advisorsbiztech@gmail.com>
2025-01-25 16:39:56 +0530

    Initial commit for backend

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose =require('mongoose');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const authRouter =require ('./routes/auth/auth-routes'); 
+const adminProductsRouter = require('./routes/admin/products-routes')
+
+//create a database connection -> u can also
+//create a separate file for this and then import/use that file
+
+mongoose.connect(
+   "mongodb+srv://rvarfa93:radhika982676@cluster0.5a8ym.mongodb.net/"
+).then(()=>console.log("MongoDB connected")).catch(error=>console.log(error));
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+    cors({
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST', "DELETE", "PUT"],
+        allowedHeaders: [
+            'Content-Type',
+            'Authorization',
+            'Cache-Control',
+            'Expires',
+            'Pragma'
+        ],
+        credentials: true,
+    })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use('/api/auth',authRouter);
+app.use('/api/admin/products',adminProductsRouter);
+
+app.listen(PORT,()=>console.log(`Server is now running on port ${PORT}`))
