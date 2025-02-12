55054d9890225995d35081d20b22b5e0b390981d
biztechadvisors <advisorsbiztech@gmail.com>
2025-02-12 11:50:20 +0530

    update latest code

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const mongoose =require('mongoose');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const authRouter =require ('./routes/auth/auth-routes'); 
+const adminOrderRouter = require("./routes/admin/order-routes");
 const adminProductsRouter = require('./routes/admin/products-routes')
+const shopProductsRouter = require("./routes/shop/products-routes");
+const shopCartRouter = require("./routes/shop/cart-routes");
+const shopAddressRouter = require("./routes/shop/address-routes");
+const shopOrderRouter = require("./routes/shop/order-routes");
 
 //create a database connection -> u can also
 //create a separate file for this and then import/use that file
@@ -34,5 +39,10 @@ app.use(cookieParser());
 app.use(express.json());
 app.use('/api/auth',authRouter);
 app.use('/api/admin/products',adminProductsRouter);
+app.use("/api/admin/orders", adminOrderRouter);
+app.use("/api/shop/products", shopProductsRouter);
+app.use("/api/shop/cart", shopCartRouter);
+app.use("/api/shop/address", shopAddressRouter);
+app.use("/api/shop/order", shopOrderRouter);
 
 app.listen(PORT,()=>console.log(`Server is now running on port ${PORT}`))
